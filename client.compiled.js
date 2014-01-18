37f81abf7948f03fdcccea80d25821799d682c59
Boris Cherny <bcherny@gmail.com>
2014-01-18 12:19:31 -0800

    update view on push over scoket

--- client.compiled.js
+++ client.compiled.js
@@ -7,25 +7,32 @@
 		io = require('socket.io/node_modules/socket.io-client'),
 		when = require('when');
 
-	load('config.json').then(init, function (err) {
-		throw err;
-	});
+	var data = [];
+
+	load('config.json').then(init, error);
 
 	function init (config) {
 
 		// fetch data
-		load(config.http.host + ':' + config.http.port).then(render, error);
+		//load(config.http.host + ':' + config.http.port).then(render, error);
 
 		// open socket
 		var socket = io.connect(config.socket.host + ':' + config.socket.port);
 
-		socket.on('push', function (data) {
-			console.log('sock: ', data);
+		socket.on('push', function (datum) {
+			push(datum);
 		});
 
 	}
 
 	// helpers
+
+	function push (datum) {
+
+		data.push(datum);
+		render(data);
+		
+	}
 
 	function render (data) {
 
